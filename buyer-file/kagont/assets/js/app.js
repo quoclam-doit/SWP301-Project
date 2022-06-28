ad0ee57050abbdd139d69697c4e0dd9826c28b78
tuanbestdaxua <tuananhtran1410@gmail.com>
2022-06-28 22:18:42 +0700

    show password

--- buyer-file/kagont/assets/js/app.js
+++ buyer-file/kagont/assets/js/app.js
@@ -9,3 +9,19 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+function show(a) {
+  var x = document.getElementById(a);
+  var c = x.nextElementSibling
+  if (x.getAttribute('type') == "password") {
+    c.removeAttribute("class");
+    c.setAttribute("class", "fas fa-eye-slash");
+    x.removeAttribute("type");
+    x.setAttribute("type", "text");
+  } else {
+    x.removeAttribute("type");
+    x.setAttribute('type', 'password');
+    c.removeAttribute("class");
+    c.setAttribute("class", "fas fa-eye");
+  }
+}
